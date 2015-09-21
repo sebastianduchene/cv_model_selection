20b2eec74c058b006791ecdf2a2985b9b1a4e694
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2015-09-21 14:49:47 +1000

    updated mb. Try beast?

--- cv_functions.R
+++ cv_functions.R
@@ -1,10 +1,10 @@
 
-phylo_likelihood <- function(nex_data_file, log_file, trees_file, test_indices = 0){
+phylo_likelihood <- function(nex_data_file, log_file, trees_file, train_indices = 0, post_samples= NULL, test_type = 'ml'){
     #Only works for GTR+G
     require(phangorn)
     data <- as.DNAbin(read.nexus.data(nex_data_file))
-    if(length(test_indices) != 0){
-        data <- as.matrix(data)[, test_indices]
+    if(length(train_indices) != 0){
+        data <- as.matrix(data)[, -train_indices]
     }
     data <- phyDat(data)
     trees <- read.nexus(trees_file)
@@ -13,12 +13,32 @@ phylo_likelihood <- function(nex_data_file, log_file, trees_file, test_indices =
     Qs <- cbind(log_file$r.A...C, log_file$r.A...G, log_file$r.A...T, log_file$r.C...G,  log_file$r.C...T, log_file$r.G...T)
     bfs <- cbind(log_file$pi.A, log_file$pi.C, log_file$pi.G, log_file$pi.T)
     log_liks <- vector()
-    for(i in 1:length(trees)){
-        tr_temp <- trees[[i]]
-        log_temp <-  pml(tr_temp, data, Q = Qs[i, ], bf = bfs[i, ])
-        log_liks[i] <- log_temp$logLik
+    if(is.null(post_samples)){
+        samples <- tail(1:nrow(log_file), 200)
+    }else{
+        samples <- post_samples
     }
-    return(log_liks)
+
+    if(test_type != 'ml'){
+        for(i in samples){
+            tr_temp <- trees[[i]]
+            log_temp <-  pml(tr_temp, data, Q = Qs[i, ], bf = bfs[i, ])
+            log_liks <- c(log_liks, log_temp$logLik)
+        }
+    }else{
+        #find highest lik row. This is giving the model its best chance at predicting future observations
+        i_ml <- which.max(log_file$LnL)
+        tree_ml <- trees[[i_ml]]
+        lik_test <- pml(tree_ml, data, Q = colMeans(Qs), bf = colMeans(bfs))
+    }
+
+    if(test_type == 'deltaL'){
+        return(abs(median(log_liks) - median(log_file$LnL[samples])))
+    }else if(test_type == 'testL'){
+        return(log_liks)
+    }else if(test_type == 'ml'){
+        return(lik_test$logLik)
+    }
 }
 
 #phylo_likelihood('ss/new_SC_UCL_3.nex', 'ss/new_SC_UCL_train3.nex.p', 'ss/new_SC_UCL_train3.nex.t', test_indices = c(1, 2, 3))
--- sims_r2/test.nexus.ss
+++ sims_r2/test.nexus.ss
@@ -1,27 +1,7 @@
 [LEGEND: The file contains statistics on the Steppingstone Sampling.]
-[ID: 6783424998]
+[ID: 6803731100]
 [   Step                --  Index of the step ]
 [   Power               --  At each step we sample from the distribution with density (Likelihood^Power)*Prior ]
 [   runX                --  Contribution to the marginal log likelihood of run X, i.e. marginal log likelihood for run X is the sum across all steps in column runX.   ]
 [   aSplitX             --  Average standard deviation of split frequencies of tree X. -2.0 is printed if no diagnostics was requested. -1.0 is printed if there were no splits with frequency above minimum.]
 Step	Power	run1	run2	aSplit0
-  1	0.8796	-3258.870721	-3259.143586	0.026612
-  2	0.7684	-3011.567569	-3011.988374	0.035917
-  3	0.6661	-2772.102873	-2772.668363	0.038777
-  4	0.5724	-2538.601361	-2538.228298	0.055504
-  5	0.4871	-2313.091806	-2313.537689	0.047834
-  6	0.4100	-2094.065087	-2094.058394	0.059756
-  7	0.3406	-1882.205910	-1882.585547	0.062475
-  8	0.2789	-1679.157540	-1679.295761	0.062854
-  9	0.2243	-1484.479969	-1483.350490	0.076192
- 10	0.1768	-1298.038276	-1295.584533	0.077757
- 11	0.1358	-1119.313451	-1118.057617	0.077782
- 12	0.1012	-951.570299	-951.693645	0.100325
- 13	0.0725	-790.563882	-790.499351	0.114722
- 14	0.0493	-644.768994	-643.822303	0.087029
- 15	0.0312	-509.376442	-512.821105	0.109994
- 16	0.0179	-397.733336	-389.210368	0.128978
- 17	0.0087	-298.073781	-289.440770	0.126089
- 18	0.0032	-219.228779	-202.843162	0.127798
- 19	0.0006	-111.985509	-112.934963	0.132928
- 20	0.0000	-26.537663	-25.541173	0.131550
