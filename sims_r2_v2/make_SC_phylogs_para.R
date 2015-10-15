8d2c60f25245fb8a0c9f14ab7f4791ec33d06fbc
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2015-10-15 13:03:34 +1100

    updated code

---
+++ sims_r2_v2/make_SC_phylogs_para.R
@@ -0,0 +1,33 @@
+library(ape)
+library(NELSI)
+library(foreach)
+library(doParallel)
+
+
+
+proc_trees <- function(r){
+    log_temp <- read.table(strict_clocks[r], head = T)
+    log_temp <- log_temp[(nrow(log_temp) - 100):nrow(log_temp), ]
+    tree_temp <- read.nexus(gsub('log', 'trees', strict_clocks[r]))
+    tree_temp <- tree_temp[(length(tree_temp) - 100):length(tree_temp)] 
+    
+    phylogs_temp <- list()
+    for(t in 1:length(tree_temp)){
+        phylogs_temp[[t]] <- tree_temp[[t]]
+        phylogs_temp[[t]]$edge.length <- tree_temp[[t]]$edge.length * log_temp$clockRate[t]
+    }
+    class(phylogs_temp) <- 'multiPhylo'
+    write.tree(phylogs_temp, file = gsub('[.]log', '_phylogs.trees', strict_clocks[r]))
+}
+
+strict_clocks <- paste0('runs/', dir('runs/', pattern = 'strict[.]log'))
+
+# modify to run only last 100 trees
+#for(r in 1:length(strict_clocks)){
+
+cl <- makeCluster(7)
+registerDoParallel(cl)
+
+get_phylogs <- foreach(x = 1:length(strict_clocks), .packages = 'ape') %dopar% proc_trees(x)
+
+stopCluster(cl)
---
+++ sims_r2_v2/make_phylogs.py
@@ -0,0 +1,44 @@
+#!//anaconda/bin/python
+
+#/Users/sebastianducheneAIr/anaconda/bin/python
+
+print 'using new function to process only last 100 trees'
+
+import dendropy as dp
+import sys, re, copy
+
+def get_phylogs(trees_in):
+    '''
+    The trees should be read as a Tree list in dendropy
+    ''' 
+    trees_out = list()
+
+    for t in trees_in:
+        t_new = copy.deepcopy(t)
+        
+        for b in t_new.postorder_edge_iter():
+            b.length = b.length * float(b.head_node.annotations['rate'])
+
+        trees_out.append(t_new.as_string('newick'))
+
+    return(trees_out)
+
+input_file = sys.argv[1]
+
+out_file = re.sub('[.].+$', '_phylogs.trees', input_file)
+print out_file
+
+trees_in_comp = dp.TreeList.get_from_path(input_file, 'nexus', extract_comment_metadata = True)
+
+# Modified code to run only the last 100 trees
+trees_in = trees_in_comp[(len(trees_in_comp) - 100):len(trees_in_comp)]
+
+trs = get_phylogs(trees_in)
+
+
+output = open(out_file, 'w')
+for t in trs:
+    output.write(t)
+output.close()
+
+
